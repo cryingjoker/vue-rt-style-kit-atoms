a0c8e8fced19df490b9c938b9b89b8e37d6a909d
cryingjoker <cryingjoker@me.com>
2020-02-04 15:10:54 +0300

    refactor multiselect component

--- src/components/Select/SelectOption.vue
+++ src/components/Select/SelectOption.vue
@@ -1,90 +1,97 @@
 <script type="text/jsx">
-  export default {
-    inject: ["RtSelect"],
-    name: "RtSelectOption",
-    props: {
-      value: {
-        type: String,
-        default: null
-      }
-    },
-    data() {
-      return {
-        localValue: this.text ? this.text : "",
-        isSelected: false
-      };
-    },
-    computed: {
-      selectedValue() {
-        if (this.RtSelect) {
-          return this.RtSelect.selectedValue;
-        } else {
-          return "";
-        }
-      },
-      optionClasses() {
-        let optionClasses = "select-option";
-        if(this.isSelected && !this.$parent._props.multi) {
-          optionClasses += " select-option--select"
-        }
-        if(this.$parent._props.multi) {
-          optionClasses += " select-option--multi"
-        }
-        return optionClasses;
-      }
-    },
+    export default {
+        inject: ["RtSelect"],
+        name: "RtSelectOption",
+        props: {
+            value: {
+                type: String,
+                default: null
+            }
+        },
+        data() {
+            return {
+                localValue: this.text ? this.text : "",
+                isSelected: false
+            };
+        },
+        computed: {
+            selectedValue() {
+                if (this.RtSelect) {
+                    return this.RtSelect.selectedValue;
+                } else {
+                    return "";
+                }
+            },
+            optionClasses() {
+                const optionClasses = ['select-option'];
+                if (this.RtSelect && this.RtSelect.hasMultiSelect) {
+                    optionClasses.push("select-option--multi");
+                } else {
+                    if (this.isSelected) {
+                        optionClasses.push('select-option--select');
+                    }
+                }
 
-    watch: {
-      selectedValue() {
-        this.setIsSelected();
-      }
-    },
-    mounted() {
-      this.setValue();
-      this.setIsSelected();
-    },
-    methods: {
-      setIsSelected() {
-        this.isSelected = this.selectedValue === this.text;
-      },
-      setValue() {
-        this.text = this.getTextContent();
-      },
-      setSelection(e) {
-        this.RtSelect.setValue({ text: this.text, value: this.value });
-        if(this.$el.querySelector('.select-option__checkbox input[type="checkbox"]')) {
-          this.$el.querySelector('.select-option__checkbox input[type="checkbox"]').click();
-        }
-        e.preventDefault();
-      },
-      getTextContent() {
-        if (this.$el) {
-          return this.$el.textContent.trim();
-        }
-        const slot = this.$slots.default;
-        return slot ? slot[0].text.trim() : "";
-      }
-    },
-    render(h) {
-      const hiddenCheckbox = (() => {
-        if(this.$parent._props.multi) {
-          return <div class="select-option__checkbox" ref="multiSelectCheckbox">
-            <input type="checkbox" class="select-option__hidden-checkbox"/>
-            <div class="select-option__checked-sign">
-              <svg width="12" height="10" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 5l3.448 3L11 2" stroke="#70F" stroke-width="3" fill="none" fill-rule="evenodd"/>
-              </svg>
+                return optionClasses.join(' ');
+            }
+        },
+
+        watch: {
+            selectedValue() {
+                this.setIsSelected();
+            }
+        },
+        mounted() {
+            this.setValue();
+            this.setIsSelected();
+        },
+        methods: {
+            setIsSelected() {
+                this.isSelected = this.selectedValue === this.text;
+            },
+            setValue() {
+                this.text = this.getTextContent();
+            },
+            setSelection(e) {
+                this.RtSelect.setValue({text: this.text, value: this.value});
+                if (this.$refs['multiSelectCheckboxInput']) {
+                    this.$refs['multiSelectCheckboxInput'].checked = !this.$refs['multiSelectCheckboxInput'].checked;
+                }
+                e.preventDefault();
+            },
+            getTextContent() {
+                if (this.$el) {
+                    return this.$el.textContent.trim();
+                }
+                const slot = this.$slots.default;
+                return slot ? slot[0].text.trim() : "";
+            }
+        },
+        render(h) {
+            const hiddenCheckbox = (() => {
+
+                if (this.RtSelect && this.RtSelect.hasMultiSelect) {
+                    return <div class="select-option__checkbox" ref="multiSelectCheckbox">
+                        <input type="checkbox" class="select-option__hidden-checkbox" ref="multiSelectCheckboxInput"/>
+                        <div class="select-option__checkbox-angle">
+                            <svg class="select-option__checkbox-angle-icon" width="12" height="10" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 5l3.448 3L11 2" stroke="#70F" stroke-width="3" fill="none"
+                                      fill-rule="evenodd"/>
+                            </svg>
+                        </div>
+                    </div>
+                } else {
+                    return null
+                }
+            })();
+
+            return <div class={this.optionClasses}>
+
+                <button class="select-option__inner" onClick={this.setSelection}>
+                    {hiddenCheckbox}
+                    {this.$slots.default}
+                </button>
             </div>
-          </div>
         }
-      })();
-
-      return <div class={this.optionClasses}>
-        {hiddenCheckbox}
-        <button class="select-option__inner" onClick={this.setSelection}>
-          {this.$slots.default}
-        </button>
-      </div>
-    }
-  };
+    };
 </script>
