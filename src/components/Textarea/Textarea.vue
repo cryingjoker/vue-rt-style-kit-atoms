c0b04fdba9714bf545a1a65f100fa7eaede8ed1e
cryingjoker <cryingjoker@me.com>
2020-02-27 12:22:10 +0300

    refactor textarea + remove issue with value props + textarea -> jsx

--- src/components/Textarea/Textarea.vue
+++ src/components/Textarea/Textarea.vue
@@ -1,126 +1,148 @@
-<template>
-  <div :class="textareaClasses" class="text-field textarea">
-    <textarea
-      class="textarea-element"
-      rows="1"
-      @change="inputHandler"
-      @keyup="calculateHeight"
-      @input="calculateHeight"
-      :id="fieldId"
-    />
-    <div v-if="outlined" class="text-field__border"></div>
-    <div v-else class="text-field__line"></div>
-    <div
-      v-if="!!placeholder"
-      :class="placeholderClasses"
-      class="floating-placeholder"
-    >
-      {{ placeholder }}
-    </div>
-    <p class="text-field__error-message">
-      {{ errorMessage }}
-    </p>
-  </div>
-</template>
+<script type="text/jsx">
+    export default {
+        name: "RtTextarea",
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            placeholder: {
+                type: String,
+                default: null
+            },
+            hasError: {
+                type: Boolean,
+                default: false
+            },
+            errorMessage: {
+                default: String,
+                default: ""
+            },
+            value: {
+                type: String,
+                default: null
+            },
+            isB2bTextarea: {
+                type: Boolean,
+                default: false
+            },
+            outlined: {
+                type: Boolean,
+                default: false
+            },
+            color: {
+                type: String,
+                default: "purple"
+            },
+            fieldId: {
+                type: String,
+                default: ''
+            }
+        },
+        data: () => ({
+            inputText: "",
+            hasInputText: false
+        }),
+        computed: {
+            textareaClasses() {
+                const classes = ['text-field', 'textarea'];
+                if (this.disabled) {
+                    classes.push("textarea--disabled")
+                }
+                if (this.hasInputText) {
+                    classes.push("textarea--not-empty")
+                }
+                if (this.hasError) {
+                    classes.push("text-field--error")
+                }
+                if (this.isB2bTextarea) {
+                    classes.push("rtb-textarea")
+                }
+                if (this.outlined) {
+                    classes.push("rtb-textarea--outlined")
+                }
+                if (this.color === "orange") {
+                    classes.push("text-field--orange")
+                }
+                if (this.color === "purple") {
+                    classes.push("text-field--purple")
+                }
+                return classes.join(' ')
+            },
+            placeholderClasses() {
+                const classes = ['floating-placeholder']
+                if (this.hasInputText) {
+                    classes.push("floating-placeholder--go-top");
+                }
+                return classes.join(' ')
 
-<script>
-export default {
-  name: "RtTextarea",
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    placeholder: {
-      type: String,
-      default: null
-    },
-    hasError: {
-      type: Boolean,
-      default: false
-    },
-    errorMessage: {
-      default: String,
-      default: ""
-    },
-    value: {
-      type: String,
-      default: null
-    },
-    isB2bTextarea: {
-      type: Boolean,
-      default: false
-    },
-    outlined: {
-      type: Boolean,
-      default: false
-    },
-    color: {
-      type: String,
-      default: "purple"
-    },
-    fieldId: {
-      type: String,
-      default: ''
-    }
-  },
-  data: () => ({
-    inputText: "",
-    hasInputText: false
-  }),
-  computed: {
-    textareaClasses() {
-      return {
-        "textarea--disabled": this.disabled,
-        "textarea--not-empty": this.hasInputText,
-        "text-field--error": this.hasError,
-        "rtb-textarea": this.isB2bTextarea,
-        "rtb-textarea--outlined": this.outlined,
-        "text-field--orange": this.color === "orange",
-        "text-field--purple": this.color === "purple"
-      };
-    },
-    placeholderClasses() {
-      return {
-        "floating-placeholder--go-top": this.hasInputText
-      };
-    }
-  },
-  watch: {
-    localValue(val) {
-      this.$emit("change", val);
-    }
-  },
-  mounted: function() {
-    this.setValue();
-    this.setDisabled();
-  },
-  methods: {
-    setValue() {
-      this.$el.querySelector(".textarea-element").value = this.localValue || "";
-      this.setValueLength();
-    },
-    setDisabled() {
-      this.$el.querySelector(".textarea-element").disabled = Boolean(
-        this.disabled
-      );
-    },
-    setValueLength() {
-      this.hasInputText = this.localValue ? this.localValue.length > 0 : false;
-    },
-    inputHandler($event) {
-      this.localValue = this.$el.querySelector(".textarea-element").value;
-      this.setValueLength();
-    },
-    calculateHeight() {
-      const textarea = this.$el.querySelector(".textarea-element");
-      textarea.style.height = "";
-      textarea.style.height = textarea.scrollHeight + 'px';
-    },
-    clearInput() {
-      this.localValue = "";
-      this.setValue();
-    }
-  }
-};
+            }
+        },
+        watch: {
+            localValue(val) {
+                this.$emit("change", val);
+            }
+        },
+        mounted: function () {
+            this.localValue = this.value;
+            this.setValueLength();
+            this.setDisabled();
+        },
+        methods: {
+
+            setDisabled() {
+                this.$el.querySelector(".textarea-element").disabled = Boolean(
+                    this.disabled
+                );
+            },
+            setValueLength() {
+                this.hasInputText = this.localValue ? this.localValue.length > 0 : false;
+            },
+            inputHandler() {
+                this.localValue = this.$el.querySelector(".textarea-element").value;
+                this.setValueLength();
+            },
+            calculateHeight() {
+                const textarea = this.$el.querySelector(".textarea-element");
+                textarea.style.height = "";
+                textarea.style.height = textarea.scrollHeight + 'px';
+            },
+            clearInput() {
+              this.$refs.textarea.value = ''
+              this.inputHandler()
+            }
+        },
+        render(h) {
+            const renderLine = () => {
+                if (this.outlined) {
+                    return <div class="text-field__border"></div>
+
+                } else {
+                    return <div class="text-field__line"></div>
+                }
+            }
+            const renderPlaceholder = () => {
+                if (this.placeholder && this.placeholder.length > 0) {
+                    return <div class={this.placeholderClasses}>
+                        {this.placeholder}
+                    </div>
+                }
+                return null
+            }
+            return <div class={this.textareaClasses}>
+                    <textarea class="textarea-element" rows="1"
+
+                              onChange={this.inputHandler}
+                              onKeyup={this.calculateHeight}
+                              onInput={this.calculateHeight}
+                              id={this.fieldId}
+                    >{this.value}</textarea>
+                {renderLine()}
+                {renderPlaceholder()}
+                <p class="text-field__error-message">
+                    {this.errorMessage}
+                </p>
+            </div>
+        }
+    };
 </script>
