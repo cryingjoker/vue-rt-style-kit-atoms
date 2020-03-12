ce2a72e3b2cc0fb79d1bdac8600a9b2390508abc
cryingjoker <cryingjoker@me.com>
2020-03-12 15:44:49 +0300

    refactor banners + add deviceTypeStore + add variablesMixin

---
+++ src/mixins/variablesMixin.ts
@@ -0,0 +1,30 @@
+import variables from "vue-rt-style-kit-atoms/src/variables.json";
+const _normalizeVariables = {};
+(function(){
+    if(Object.keys(_normalizeVariables).length === 0) {
+        Object.keys(variables).forEach((key) => {
+            if (!parseInt(variables[key])) {
+                return
+            }
+            let newKey = key;
+            if (newKey.search('-')) {
+                newKey = newKey.split('-').map((item, index) => {
+                    if (index > 0) {
+                        item = item[0].toUpperCase() + item.substr(1)
+                    }
+                    return item
+                }).join('')
+            }
+            _normalizeVariables[newKey] = parseInt(variables[key]);
+        })
+    }
+})();
+
+const getVariable = (variableName)=>{
+    if(variableName in _normalizeVariables){
+        return _normalizeVariables[variableName];
+    }
+    console.error('cannot find "%s" in variables', variableName)
+    return null;
+}
+export default getVariable;
---
+++ src/stores/deviceTypeStoreMixin.ts
@@ -0,0 +1,78 @@
+import getVariable from '../mixins/variablesMixin.ts'
+import Vue from 'vue';
+import {debounce} from 'ts-debounce';
+
+const possibleTypes = ['desktop', 'tablet', 'mobile'];
+const tabletUpperLimit = getVariable('tabletUpperLimit');
+const mobileUpperLimit = getVariable('mobileUpperLimit');
+const watchers = {};
+let deviceType = NaN;
+
+const checkWidth = () => {
+
+    const width = window.innerWidth
+
+    let newDeviceType;
+    switch (true) {
+        case width > tabletUpperLimit:
+            newDeviceType = 0;
+            break;
+        case width > mobileUpperLimit:
+            newDeviceType = 1;
+            break;
+        default:
+            newDeviceType = 2;
+    }
+    if (newDeviceType != deviceType) {
+        deviceType = newDeviceType
+        runWatchers()
+    }
+};
+const debounceCheckWidth = debounce(() => {
+    checkWidth();
+}, 30);
+let statusEventListenet = 0;
+const eventListener = (status: boolean = false) => {
+    const statusNumber: number = status ? 1 : 0;
+    if(statusNumber != statusEventListenet) {
+        const methods = ['removeEventListener', 'addEventListener']
+        window[methods[statusNumber]]('resize', debounceCheckWidth)
+        statusEventListenet = statusNumber;
+    }
+}
+
+
+const runWatchers = () => {
+    Object.values(watchers).forEach((fnArray: any) => {
+        if (fnArray && fnArray.length > 0) {
+            fnArray.forEach((fn) => {
+                fn.call();
+            })
+        }
+    })
+};
+const addWatcher = (id, fn) => {
+    if (!watchers[id]) {
+        watchers[id] = []
+    }
+    watchers[id].push(fn);
+    eventListener(Object.keys(watchers).length > 0)
+};
+const removeWatcher = (id) => {
+    delete watchers[id];
+
+    eventListener(Object.keys(watchers).length > 0)
+};
+const getStatus = () => {
+    if(isNaN(deviceType)){
+        checkWidth();
+    }
+    return possibleTypes[deviceType];
+}
+
+export const deviceTypeStore =  Vue.observable({
+    getStatus: getStatus,
+    removeWatcher: removeWatcher,
+    addWatcher: addWatcher
+});
+
