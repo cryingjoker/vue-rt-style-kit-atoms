53dbd5459cddaf39fc36963a3ebdd45b71ff87da
cryingjoker <cryingjoker@me.com>
2020-07-03 15:02:39 +0300

    add new style + update GridColumn.vue

--- src/components/Grid/GridColumn.vue
+++ src/components/Grid/GridColumn.vue
@@ -1,76 +1,91 @@
 <script type="text/jsx">
 
 
-    import {getSpacesClass, spacesParamsNames, spacesParamsProps} from "../../mixins/spaceClassMixin.js";
-    import {getDisplayClass, displayParamsNames, displayParamsProps} from "../../mixins/displayClassMixin.js";
+  import {getSpacesClass, spacesParamsNames, spacesParamsProps} from "../../mixins/spaceClassMixin.js";
+  import {getDisplayClass, displayParamsNames, displayParamsProps} from "../../mixins/displayClassMixin.js";
 
-    const componentProps = {
-        size: {
-            type: [Number, String],
-            default: 0
-        },
-        tabletSize: {
-            type: [Number, String],
-            default: -1
-        },
-        mobileSize: {
-            type: [Number, String],
-            default: -1
-        }
+  const componentProps = {
+    size: {
+      type: [Number, String],
+      default: 0
+    },
+    tabletSize: {
+      type: [Number, String],
+      default: -1
+    },
+    mobileSize: {
+      type: [Number, String],
+      default: -1
     }
-    const componentsList = {};
-    export default {
-        name: "RtCol",
-        props: {...componentProps, ...spacesParamsProps, ...displayParamsProps},
-        data() {
-            return {
-                sizeParams: ['size', 'mobileSize', 'tabletSize'],
-            }
-        },
+  }
+  const componentsList = {};
+  export default {
+    name: "RtCol",
+    props: {...componentProps, ...spacesParamsProps, ...displayParamsProps},
+    data() {
+      return {
+        sizeParams: ['size', 'mobileSize', 'tabletSize'],
+      }
+    },
 
-        mounted() {
-        },
-        computed: {
-            rowClassName() {
-                const classNamesArray = [...spacesParamsNames.map((name) => {
-                    if (this[name] >= 0) {
-                        return getSpacesClass(name, this[name]);
-                    }
-                }), ...displayParamsNames.map((name) => {
-                    if (this[name]) {
-                        return getDisplayClass(name, this[name]);
-                    }
-                }), ...this.sizeParams.map((name)=>{
-                    if(this[name] >= 0){
-                        return this.getClassName(name, this[name])
-                    }
-                })
-                ].filter((i) => i && i.length > 0);
+    mounted() {
+    },
+    computed: {
+      rowClassName() {
+        const classNamesArray = [...spacesParamsNames.map((name) => {
+          if (this[name] >= 0) {
+            return getSpacesClass(name, this[name]);
+          }
+        }), ...displayParamsNames.map((name) => {
+          if (this[name]) {
+            return getDisplayClass(name, this[name]);
+          }
+        }), ...this.sizeParams.map((name) => {
+          if (this[name] >= 0) {
+            return this.getClassName(name, this[name])
+          }
+        })
+        ].filter((i) => i && i.length > 0);
 
 
-                return classNamesArray.join(' ');
-            }
-        },
-        methods: {
-            getClassName(name, value){
-                const classNameArray = ['rt','col'];
-                if(value > 0) {
-                switch (true) {
-                    case name.search(/mobile/i)>=0:
-                        classNameArray.push('md');
-                        break;
-                    case name.search(/tablet/i)>=0:
-                        classNameArray.push('td');
-                        break;
-                }
-                    classNameArray.push(value.toString());
-                }
-                return classNameArray.join('-');
-            }
-        },
-        render(h) {
-            return <div class={this.rowClassName}>{this.$slots.default}</div>
+        return classNamesArray.join(' ');
+      }
+    },
+    methods: {
+      getClassName(name, value) {
+        let classNameArray = ['rt', 'col'];
+        console.info('value',value)
+        if (value > 0) {
+          switch (true) {
+            case name.search(/mobile/i) >= 0:
+              classNameArray.push('md');
+              break;
+            case name.search(/tablet/i) >= 0:
+              classNameArray.push('td');
+              break;
+          }
+          classNameArray.push(value.toString());
         }
+        if (value == 0) {
+          classNameArray = [];
+          switch (true) {
+            case name.search(/mobile/i) >= 0:
+              classNameArray.push('md');
+              break;
+            case name.search(/tablet/i) >= 0:
+              classNameArray.push('td');
+              break;
+          }
+          classNameArray.push('d')
+          classNameArray.push('none')
+        }
+        console.info(classNameArray.join('-'))
+        return classNameArray.join('-');
+      }
+    },
+    render(h) {
+      return <div class={this.rowClassName}>{this.$slots.default}</div>
+    }
 
-    };
+  };
 </script>
