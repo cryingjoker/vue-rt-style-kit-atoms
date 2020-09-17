64160c465a24f9cf84fc153623a3e9374b249625
cryingjoker <cryingjoker@me.com>
2020-09-17 11:17:16 +0300

    start new price

---
+++ src/components/Price/PriceV2.vue
@@ -0,0 +1,75 @@
+<script type="text/jsx">
+export default {
+  name: "RtPriceV2",
+  props: {
+    bright: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: [Number, String],
+      default: 0
+    },
+    oldValue: {
+      type: [Number, String],
+      default: 0
+    },
+    optionLabel: {
+      type: String,
+      default: 'от'
+    },
+    showFloat: {
+      type: Boolean,
+      default: false
+    },
+    colorValue: {
+      type: String,
+      default: ''
+    },
+    currency: {
+      type: String,
+      default: 'руб'
+    },
+    timeInterval: {
+      type: String,
+      default: 'мес.'
+    },
+    isOption: {
+      type: Boolean,
+      default: false
+    },
+    onlyPrice: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    localValue: 0,
+    normalizeValue: 0,
+    normalizeOldValue: 0,
+    priceType: "",
+    normalizeCurrency: '',
+    normalizeTimeInterval: 0,
+  }),
+  watch: {},
+  camputed: {
+    normalValue(){
+      let value = (this.value + '')
+
+    }
+  },
+
+
+  mounted() {
+
+
+  },
+  computed: {},
+  render(h) {
+    return <div class="rt-price-v2">new price</div>
+  }
+
+
+}
+
+</script>
