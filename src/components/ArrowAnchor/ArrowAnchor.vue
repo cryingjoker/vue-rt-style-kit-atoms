76952fb9f6679427961d275afc02176b58c7be9d
cryingjoker <cryingjoker@me.com>
2020-10-27 11:49:22 +0300

    add iconColor props in jumbotron

--- src/components/ArrowAnchor/ArrowAnchor.vue
+++ src/components/ArrowAnchor/ArrowAnchor.vue
@@ -12,6 +12,10 @@ export default {
     scrollToId: {
       type: String,
       default: ''
+    },
+    iconColor:{
+      type: String,
+      default: ''
     }
   },
   data: () => ({}),
@@ -20,7 +24,7 @@ export default {
   render: function (h) {
     return <button v-rt-scroll-to-on-click={{scrollToId: this.scrollToId}} type="button"
                    class="d-flex rt-sys-icon-hover--orange rt-sys-icon-focus--orange-button-active  rt-arrow-achor">
-      <rt-system-icons class="rt-arrow-achor-icon" name="arrow right"></rt-system-icons>
+      <rt-system-icons class="rt-arrow-achor-icon" name="arrow right" color={this.iconColor}></rt-system-icons>
       <span class="sp-l-0-4 rt-font-control">{this.$slots.default}</span></button>
   }
 };
