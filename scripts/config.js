8385343d730386f69ea9b62b3cc37812c945cd2b
cryingjoker <cryingjoker@me.com>
2021-04-01 13:50:41 +0300

    added support node version more than v12.14

--- scripts/config.js
+++ scripts/config.js
@@ -1,32 +1,43 @@
-const path = require('path')
-const fs = require('fs');
-const babel = require('rollup-plugin-babel');
-const babelCompilerPlugin = require('./babel-compiler-plugin');
-const cjs = require('@rollup/plugin-commonjs')
-const vue = require('rollup-plugin-vue');
-const node = require('@rollup/plugin-node-resolve')
-const flow = require('rollup-plugin-flow-no-whitespace')
-const postcss = require('rollup-plugin-postcss')
-const postcssEnv = require('postcss-preset-env');
-const externals = require('rollup-plugin-node-externals')
-// const typescript = require('@rollup/plugin-typescript');
-// const typescript = require('rollup-plugin-typescript2');
-const stylusCompilerPlugin = require('./rollup-plugin-stylus-compiler');
-const stylusMixin = require('./stylus-mixin')
-const terser = require("rollup-plugin-terser").terser;
-const css = require('rollup-plugin-css-porter');
-const cssnano = require('cssnano')
-const version = process.env.VERSION || require('../package.json').version
+import path from 'path'
+
+import babel from 'rollup-plugin-babel'
+import cjs from '@rollup/plugin-commonjs'
+import vue from 'rollup-plugin-vue'
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+import flow from 'rollup-plugin-flow-no-whitespace'
+import postcss from 'rollup-plugin-postcss'
+import postcssEnv from 'postcss-preset-env'
+import externals from 'rollup-plugin-node-externals'
+import stylusCompilerPlugin from './rollup-plugin-stylus-compiler.js'
+import stylusMixin from './stylus-mixin.js'
+import {terser} from "rollup-plugin-terser"
+import css from 'rollup-plugin-css-porter'
+import cssnano from 'cssnano'
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear()
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+const version = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
 const aliases = {}
-const json = require('rollup-plugin-json');
-const alias = require('@rollup/plugin-alias');
+import json from 'rollup-plugin-json'
+import alias from '@rollup/plugin-alias'
+
 const resolve = p => {
-    const base = p.split('/')[0]
-    if (aliases[base]) {
-        return path.resolve(aliases[base], p.slice(base.length + 1))
-    } else {
-        return path.resolve(__dirname, '../', p)
-    }
+  const base = p.split('/')[0]
+  if (aliases[base]) {
+    return path.resolve(aliases[base], p.slice(base.length + 1))
+  } else {
+    return path.resolve(__dirname, '../', p)
+  }
 }
 const banner =
     '/*!\n' +
@@ -35,138 +46,138 @@ const banner =
     ' * Released under the MIT License.\n' +
     ' */';
 const builds = {
-    'web-full-prod': {
-        entry: resolve('src/index.js'),
-        dest: resolve('lib/vue-rt-style-kit-atoms.js'),
-        destCss: resolve('lib/vue-rt-style-kit-atoms.css'),
-        destCssPath: resolve('lib/'),
-        format: 'umd',
-        env: 'production',
-        sourcemap: true,
-        banner,
-        external: ['vue2-datepicker'],
+  'web-full-prod': {
+    entry: resolve('src/index.js'),
+    dest: resolve('lib/vue-rt-style-kit-atoms.js'),
+    destCss: resolve('lib/vue-rt-style-kit-atoms.css'),
+    destCssPath: resolve('lib/'),
+    format: 'umd',
+    env: 'production',
+    sourcemap: true,
+    banner,
+    external: ['vue2-datepicker'],
     
-        assetPath: resolve('lib'),
-
-    },
-    'web-es-full-prod': {
-        entry: resolve('src/index.js'),
-        dest: resolve('lib/vue-rt-style-kit-atoms.esm.js'),
-        destCss: resolve('lib/vue-rt-style-kit-atoms.css'),
-        destCssPath: resolve('lib/'),
-        format: 'es',
-        env: 'production',
-        sourcemap: true,
-        banner,
-        external: ['vue2-datepicker'],
+    assetPath: resolve('lib'),
     
-        assetPath: resolve('lib'),
-
-    },
+  },
+  'web-es-full-prod': {
+    entry: resolve('src/index.js'),
+    dest: resolve('lib/vue-rt-style-kit-atoms.esm.js'),
+    destCss: resolve('lib/vue-rt-style-kit-atoms.css'),
+    destCssPath: resolve('lib/'),
+    format: 'es',
+    env: 'production',
+    sourcemap: true,
+    banner,
+    external: ['vue2-datepicker'],
+    
+    assetPath: resolve('lib'),
+    
+  },
 }
 
 function genConfig(name = 'web-full-prod') {
-    const opts = builds[name];
-    const config = {
-        input: opts.entry,
-        external: opts.external,
+  const opts = builds[name];
+  const config = {
+    input: opts.entry,
+    external: opts.external,
+    plugins: [
+      alias({
+        entries: [
+          {find: '@projectAtoms', replacement: '../package.json'},
+        ]
+      }),
+      flow(),
+      
+      json(),
+      externals({
+        include: [
+          'vue',
+        ],
+        exclude: [
+          'debounce'
+        ]
+      }),
+      
+      stylusCompilerPlugin({
+        fn: stylusMixin,
+      }),
+      
+      postcss({
+        inject: false,
+        minimize: true,
+        extract: true,
         plugins: [
-            alias({
-                entries: [
-                    { find: '@projectAtoms', replacement: '../package.json' },
-                ]}),
-            flow(),
-
-            json(),
-            externals({
-                include: [
-                    'vue',
-                ],
-                exclude:[
-                    'debounce'
-                ]
-            }),
-
-            stylusCompilerPlugin({
-                fn: stylusMixin,
-            }),
-
-            postcss({
-                inject: false,
-                minimize: true,
-                extract: true,
-                plugins: [
-                    postcssEnv({
-                        stage: 3,
-                        features: {
-                            'nesting-rules': true,
-                        },
-                        exportTo: opts.destCss
-                    }),
-                    cssnano()
-                ],
-            }),
-            // typescript(),
-            // babelCompilerPlugin(),
-            babel({
-                babelrc: true
-            }),
+          postcssEnv({
+            stage: 3,
+            features: {
+              'nesting-rules': true,
+            },
+            exportTo: opts.destCss
+          }),
+          cssnano()
+        ],
+      }),
+      // typescript(),
+      // babelCompilerPlugin(),
+      babel({
+        babelrc: true
+      }),
+      
+      
+      nodeResolve({
+        default: {
+          jsnext: true
+        }
+      }),
+      
+      terser(),
+      
+      
+      vue(),
+      cjs(),
     
     
-    
-    
-    
-            node.default({
-                jsnext: true
-            }),
-
-            terser(),
-
-
-            vue(),
-            cjs(),
-
-
-        ].concat(opts.plugins || []),
-        output: {
-            file: opts.dest,
-            format: opts.format,
-            banner: opts.banner,
-            name: opts.moduleName || 'vue-rt-style-kit-atoms',
-            sourcemap: true,
-            // dir: resolve('lib')
-        },
-        onwarn: (msg, warn) => {
-            if (!/Circular/.test(msg)) {
-                warn(msg)
-            }
-        },
-    }
-    // console.info('config',config)
-    const vars = {
-        __VERSION__: version
-    }
-    // feature flags
-    // Object.keys(featureFlags).forEach(key => {
-    //     vars[`process.env.${key}`] = featureFlags[key]
-    // })
-    // build-specific env
-
-    // config.plugins.push(replace(vars))
-
-    // if (opts.transpile !== false) {
-    //     config.plugins.push(buble())
-    // }
-
-    Object.defineProperty(config, '_name', {
-        enumerable: false,
-        value: name
-    })
-
-    return {
-        config: config,
-        opts: opts
-    }
+    ].concat(opts.plugins || []),
+    output: {
+      file: opts.dest,
+      format: opts.format,
+      banner: opts.banner,
+      name: opts.moduleName || 'vue-rt-style-kit-atoms',
+      sourcemap: true,
+      // dir: resolve('lib')
+    },
+    onwarn: (msg, warn) => {
+      if (!/Circular/.test(msg)) {
+        warn(msg)
+      }
+    },
+  }
+  // console.info('config',config)
+  const vars = {
+    __VERSION__: version
+  }
+  // feature flags
+  // Object.keys(featureFlags).forEach(key => {
+  //     vars[`process.env.${key}`] = featureFlags[key]
+  // })
+  // build-specific env
+  
+  // config.plugins.push(replace(vars))
+  
+  // if (opts.transpile !== false) {
+  //     config.plugins.push(buble())
+  // }
+  
+  Object.defineProperty(config, '_name', {
+    enumerable: false,
+    value: name
+  })
+  
+  return {
+    config: config,
+    opts: opts
+  }
 }
 
-module.exports = genConfig
+export default genConfig
