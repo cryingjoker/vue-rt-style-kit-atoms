49d9e7e0a6d14b87e755d39e9b2af81fc970b3d4
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2021-06-25 16:00:44 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ VueRtStyle = {
  */
 const settingsKey = Global.globalSettingsKey;
 
-const version = "2021-06-24 15:55";
+const version = "2021-06-25 15:57";
 if (window && window.addEventListener) {
     if (settingsKey) {
         if (!window[settingsKey]) window[settingsKey] = {}
