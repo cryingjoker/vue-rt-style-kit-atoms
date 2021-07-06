1b7465b29eb5976210d49bc4001357b4316309cc
cryingjoker <cryingjoker@me.com>
2021-07-06 16:16:10 +0300

    build

--- src/index.js
+++ src/index.js
@@ -126,7 +126,7 @@ VueRtStyle = {
  */
 const settingsKey = Global.globalSettingsKey;
 
-const version = "2021-07-05 11:58";
+const version = "2021-07-06 16:15";
 if (window && window.addEventListener) {
     if (settingsKey) {
         if (!window[settingsKey]) window[settingsKey] = {}
