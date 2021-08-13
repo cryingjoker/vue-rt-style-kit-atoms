07bfed49dd31e9ff01a7125e0596cfa88f97f2ae
Ратьков Сергей <sergey.ratkov@rt.ru>
2021-08-13 16:02:35 +0300

    build

--- src/index.js
+++ src/index.js
@@ -128,7 +128,7 @@ VueRtStyle = {
  */
 const settingsKey = Global.globalSettingsKey;
 
-const version = "2021-08-13 15:23";
+const version = "2021-08-13 16:01";
 if (window && window.addEventListener) {
     if (settingsKey) {
         if (!window[settingsKey]) window[settingsKey] = {}
